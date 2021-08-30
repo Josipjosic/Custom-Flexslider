07dc1069e79fc4a531c359cf3869664b41362a3b
Josip Josic <71074064+Josipjosic@users.noreply.github.com>
2021-08-30 12:02:50 +0200

    Arrow functions
    Added arrow functions in js

--- src/functions.js
+++ src/functions.js
@@ -3,14 +3,14 @@ $(document).ready(function() {
     const item_width = $('.slider li').outerWidth();          // Width of the selected items
 	const left_value = item_width ;          // Value of how much items gonna move by   
     
-    $('.left').on("click",function(){          // On click function
+    $('.left').on("click",() => {          // On click function
 
         const left_intend_up = parseInt($('.upperRow li')) + item_width
         $('.left').prop("disabled", true);          // Button disabled
         
         $('.upperRow').animate({          
             'margin-left' : left_intend_up
-        }, function(){                           // Loop/move function by manipulating css
+        }, () => {                           // Loop/move function by manipulating css
             $('.upperRow li:last').after($('.upperRow li:first'));
             $('.left').prop("disabled", false);          // Button enabled
             $('.upperRow ul').css({
@@ -19,14 +19,14 @@ $(document).ready(function() {
         });
     });
 
-    $('.left').on("click",function(){    // On click function
+    $('.left').on("click",() => {    // On click function
    
         const left_intend_down = parseInt($('.lowerRow li')) + item_width
         $('.left').prop("disabled", true);          // Button disabled
         
         $('.lowerRow').animate({          
             'margin-left' : left_intend_down
-        }, function(){                           // Loop/move function by manipulating css
+        }, () => {                           // Loop/move function by manipulating css
             $('.lowerRow li:last').after($('.lowerRow li:first'));
             $('.left').prop("disabled", false);          // Button enabled
             $('.lowerRow ul').css({
@@ -36,13 +36,13 @@ $(document).ready(function() {
         
     }); 
 
-    $('.right').on("click",function(){          // On click function
+    $('.right').on("click",() => {          // On click function
         const left_intend_up = parseInt($('.upperRow li')) + item_width
         $('.right').prop("disabled", true);          // Button disabled
         
         $('.upperRow').animate({          
             'margin-left' : left_intend_up
-        }, function(){                           // Loop/move function by manipulating css
+        }, () => {                           // Loop/move function by manipulating css
             $('.upperRow li:first').before($('.upperRow li:last'));
             $('.right').prop("disabled", false);          // Button enabled
             $('.upperRow ul').css({
@@ -51,13 +51,13 @@ $(document).ready(function() {
         });
     });
 
-    $('.right').on("click",function(){          // On click function
+    $('.right').on("click",() =>{          // On click function
         const left_intend_down = parseInt($('.lowerRow li')) + item_width
         $('.right').prop("disabled", true);          // Button disabled 
         
         $('.lowerRow').animate({          
             'margin-left' : left_intend_down
-        }, function(){                           // Loop/move function by manipulating css
+        }, () => {                           // Loop/move function by manipulating css
             $('.lowerRow li:first').before($('.lowerRow li:last'));
             $('.right').prop("disabled", false);          // Button enabled
             $('.lowerRow ul').css({
